25f5f83c839cf7a5c785d1c888f8eb592a92667f
YASIN1881 <yasinnasiry18@gmail.com>
2024-02-19 14:41:59 +0330

    config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#377DFF",
+        green: "#38CB89",
+        orange: "#FFAB00",
+        red: "#FF5630",
+        background: "#FFFFFF",
+        seconderyBg: "#F3F5F7",
+        footerBg: "#141718",
+        textColor: "#000000",
+        mainText: "#141718",
+        primaryText: "#6C7275",
+        seconderyText: "#232627",
+      },
+    },
   },
   plugins: [],
-}
-
+};
